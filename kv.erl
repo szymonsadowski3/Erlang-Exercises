090f014b876861a291bd023c2b793549f8ffcb81
Szymon Sadowski <szymonsadowski3@gmail.com>
2017-12-14 15:04:31 +0100

    Update kv.erl

--- kv.erl
+++ kv.erl
@@ -1,8 +1,8 @@
 -module(kv).
 
--export([start/0, stop/0, store/2, lookup/1, delete/1, hash/1]).
+-export([start/0, stop/0, store/2, lookup/1, delete/1, hash/1, history/0]).
 
--export([server_loop/1]).
+-export([server_loop/2]).
 
 %---------------------------------------------------------------------------------------------
 % Client API
@@ -11,7 +11,7 @@
 % Start the KV-store.
 start() ->
     crypto:start(),
-    Pid = spawn(?MODULE, server_loop, [dict:new()]),
+    Pid = spawn(?MODULE, server_loop, [dict:new(), []]),
     register(?MODULE, Pid),
     ok.
 
@@ -26,30 +26,40 @@ lookup(K) -> rpc({lookup, K, self()}).
 % Delete an existing key K. Returns ok or not_found.
 delete(K) -> rpc({delete, K, self()}).
 
+history() -> rpc({history, self()}).
 %---------------------------------------------------------------------------------------------
 % Server Implementation
 %---------------------------------------------------------------------------------------------
-server_loop(Dict) ->
+server_loop(Dict, History) ->
     receive
         {store, K, V, Client} ->
             Dict2 = dict:store(K, V, Dict),
+            R = io_lib:format("store ~p:~p ~n", [K, V]),
+            History2 = [R] ++ History,
             Client ! {hash, K},
-            server_loop(Dict2);
+            server_loop(Dict2, History2);
         {lookup, K, Client} ->
+          R = io_lib:format("lookup ~p ~n", [K]),
+          History2 = [R] ++ History,
             Reply = case dict:find(K, Dict) of
                 {ok, Value} -> Value;
                 error       -> not_found
             end,
             Client ! {value, Reply},
-            server_loop(Dict);
+            server_loop(Dict, History2);
         {delete, K, Client} ->
+          R = io_lib:format("delete ~p ~n", [K]),
+          History2 = [R] ++ History,
             Dict2 = case dict:is_key(K, Dict) of
                 true -> dict:erase(K, Dict);
                 false -> Dict
             end,
             Client ! {deleted, ok},
-            server_loop(Dict2);
-        {stop} -> exit(shutdown)
+            server_loop(Dict2, History2);
+        {history, Client} ->
+          Client ! {value,History},
+          server_loop(Dict, History)
+        %{stop} -> exit(shutdown)
     end.
 
 %---------------------------------------------------------------------------------------------
